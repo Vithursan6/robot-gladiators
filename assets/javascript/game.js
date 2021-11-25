9c616c4e42440c74d83182c856e796c2d89e418a
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2021-11-25 17:15:25 -0500

    rand health and attack values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,7 +31,7 @@ var fight = function(enemyName) {
                             window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                     
                             //subtract money from playerMoney for skipping
-                            playerMoney = playerMoney - 10;
+                            playerMoney = Math.max(0, playerMoney - 10);
                             console.log("playerMoney", playerMoney)
                             break;
                     
@@ -41,7 +41,9 @@ var fight = function(enemyName) {
 
         
             //remove enemy's health by subtracting the amount set in the playerAttack variable
-            enemyHealth = enemyHealth - playerAttack;
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+
+            enemyHealth = Math.max(0, enemyHealth - damage);
             console.log(
                 playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
             );
@@ -63,7 +65,9 @@ var fight = function(enemyName) {
         
         
             //remove player's health by subtracting the amount set in the enemyAttack variable
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyHealth - 3, enemyAttack);
+            
+            playerHealth = Math.max(0, playerHealth - damage);
             console.log(
                 enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
                 );
@@ -107,7 +111,7 @@ var startGame = function() {
         var pickedEnemyName = enemyNames[i];
 
         //reset enemyHealth before starting new fight
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
 
         //use debugger to pause script and check
         //debugger;
@@ -236,6 +240,13 @@ var shop = function () {
      
 
 };
+
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return value;
+}
 
 
 //start the game when the page loads
